471581bfb282d059b17aa15047cb0b9d72ab70f4
aritrakoley <koley.aritra824@gmail.com>
2023-03-23 11:07:07 +0000

    Add laps feature

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -9,13 +9,15 @@ type DisplayTimeType = {
 
 type TimerStatusType = "init" | "running" | "paused"
 
+type LapType = { id: number, laptime: DisplayTimeType }
+
 const INIT_DISPLAY_TIME: DisplayTimeType = {
     hours: 0,
     minutes: 0,
     seconds: 0
 }
 
-const hms2String = (displayTime: DisplayTimeType) => `${displayTime.hours.toString().padStart(2, '0')} : ${displayTime.minutes.toString().padStart(2, '0')} : ${displayTime.seconds.toFixed(2).padStart(5, '0')}`;
+const displayTime2String = (displayTime: DisplayTimeType) => `${displayTime.hours.toString().padStart(2, '0')} : ${displayTime.minutes.toString().padStart(2, '0')} : ${displayTime.seconds.toFixed(2).padStart(5, '0')}`;
 
 const ms2HMS = (t: number, toString: boolean = false) => {
     let seconds = t / 1000;
@@ -24,18 +26,22 @@ const ms2HMS = (t: number, toString: boolean = false) => {
     seconds = (seconds % 3600) % 60;
 
     const res = { hours: hours, minutes: mins, seconds: seconds }
-    return toString ? hms2String(res) : res;
+    return toString ? displayTime2String(res) : res;
 }
 
 const StopWatch = () => {
 
-    const [displayTime, setDisplayTime] = useState<DisplayTimeType>(INIT_DISPLAY_TIME)
-    const [timerStatus, setTimerStatus] = useState<TimerStatusType>("init")
+    const [timerStatus, setTimerStatus] = useState<TimerStatusType>("init");
+    const [displayTime, setDisplayTime] = useState<DisplayTimeType>(INIT_DISPLAY_TIME);
+    const [laps, setLaps] = useState<LapType[]>([]);
 
-    const latestStartTimeRef = useRef(0)
-    const elapsedTimeRef = useRef(0)
-    const totalElapsedTimeRef = useRef(0)
-    const intervalRef = useRef(0)
+
+    const latestStartTimeRef = useRef(0);
+    const elapsedTimeRef = useRef(0);
+    const totalElapsedTimeRef = useRef(0);
+    const intervalRef = useRef(0);
+    const latestLapId = useRef(0);
+
 
 
     const updateTimer = () => {
@@ -75,34 +81,43 @@ const StopWatch = () => {
         latestStartTimeRef.current = 0;
         totalElapsedTimeRef.current = 0;
         elapsedTimeRef.current = 0;
+        latestLapId.current = 0;
 
-        setDisplayTime(INIT_DISPLAY_TIME)
-        setTimerStatus("init");
+        setDisplayTime(INIT_DISPLAY_TIME);
+        setLaps([]);
+        setTimerStatus("init");;
+    }
+
+    const saveLap = () => {
+        latestLapId.current++;
+        setLaps([...laps, { id: latestLapId.current, laptime: displayTime }])
     }
 
     return (
         <div style={{ display: 'flex' }}>
-            <div style={{ display: 'flex', flexDirection: 'column', minWidth: '200px' }}>
-                <div>{hms2String(displayTime)}</div>
+            <div style={{ display: 'flex', flexDirection: 'column', minWidth: '250px' }}>
+                <div>{displayTime2String(displayTime)}</div>
                 <div style={{ display: 'flex', gap: 5 }}>
                     {timerStatus === "init" && <button onClick={startTimer}>Start</button>}
                     {timerStatus === "paused" && <button onClick={resumeTimer}>Resume</button>}
                     {timerStatus === "running" && <button onClick={pauseTimer}>Pause</button>}
                     {timerStatus !== "init" && <button onClick={resetTimer}>Reset</button>}
+                    {timerStatus === "running" && <button onClick={saveLap}>Lap</button>}
                 </div>
-                <div>
+                {
+                    laps.length ?
+                        <div>
+                            <div>Laps:</div>
+                            <ul>
+                                {laps.map(lap => <li key={lap.id}>{`Lap ${lap.id} ::  ${displayTime2String(lap.laptime)}`}</li>)}
+                            </ul>
 
-                </div>
+                        </div> : null
+                }
             </div>
             <div>
                 <pre>
-                    {`
-                    Details:
-                    status:           ${timerStatus}
-                    latestResumeTime: ${new Date(latestStartTimeRef.current)}
-                    totalElapsedTime: ${ms2HMS(totalElapsedTimeRef.current, true)}
-                    elapsedTime:      ${ms2HMS(elapsedTimeRef.current, true)}
-                    displayTime:      ${JSON.stringify(displayTime)}
+                    {`Details:\nstatus:           ${timerStatus}\nlatestResumeTime: ${new Date(latestStartTimeRef.current)}\ntotalElapsedTime: ${ms2HMS(totalElapsedTimeRef.current, true)}\nelapsedTime:      ${ms2HMS(elapsedTimeRef.current, true)}\ndisplayTime:      ${JSON.stringify(displayTime)}
                     `}
                 </pre>
             </div>
