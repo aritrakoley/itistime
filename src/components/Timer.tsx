3b6ecbd7f447021a091f59206aa1ed63705bbcb3
aritrakoley <koley.aritra824@gmail.com>
2023-09-10 15:22:57 +0000

    UI Overhaul: Phase Config

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { ArrowUturnRightIcon, PlayIcon, PauseIcon, StopIcon, ArrowPathIcon } from '@heroicons/react/24/solid';
+import { ArrowUturnRightIcon, PlayIcon, PauseIcon, ForwardIcon, ArrowPathIcon, PlusIcon } from '@heroicons/react/24/solid';
 import { DEFAULT_SETTINGS, DEFAULT_UPDATE_INTERVAL } from '../config/constants';
 import TimeDisplay from './TimeDisplay';
 
@@ -104,88 +104,85 @@ const Timer = () => {
   };
 
 
-  const StartButton =
+  const startButton =
     <button className="group action-btn bg-green-600 active:bg-green-700 focus:ring-green-200" onClick={startTimer}>
-      <PlayIcon className="action-button-icon ml-1 mr-[-0.25rem] w-20 h-20" />
+      <PlayIcon className="action-btn-icon ml-1 mr-[-0.25rem]" />
     </button >;
 
-  const ResumeButton =
+  const resumeButton =
     <button className="group action-btn bg-green-600 active:bg-green-700 focus:ring-green-200" onClick={resumeTimer}>
-      <PlayIcon className="action-button-icon ml-1 mr-[-0.25rem] w-20 h-20" />
+      <PlayIcon className="action-btn-icon ml-1 mr-[-0.25rem]" />
     </button>;
 
 
-  const PauseButton =
+  const pauseButton =
     <button className="group action-btn bg-yellow-600 active:bg-yellow-700 focus:ring-yellow-200" onClick={pauseTimer}>
-      <PauseIcon className="action-button-icon w-20 h-20" />
+      <PauseIcon className="action-button-icon" />
     </button>;
 
-  const ResetButton =
+  const resetButton =
     <button className="group action-btn bg-cyan-600 active:bg-cyan-700 focus:ring-cyan-200" onClick={resetTimer}>
-      <ArrowPathIcon className="action-button-icon w-20 h-20" />
+      <ArrowPathIcon className="action-btn-icon" />
     </button>;
+
+  const autoNextToggle = <button className={`group ${settings.autostart_next_phase ? 'toggle-btn-on' : 'toggle-btn-off'}`} onClick={() => setSettings(prev => ({ ...prev, autostart_next_phase: !prev.autostart_next_phase }))}>
+    <ForwardIcon className="toggle-btn-icon" />
+  </button >;
+
+  const loopToggle = <button className={`group ${settings.loop ? 'toggle-btn-on' : 'toggle-btn-off'}`} onClick={() => setSettings(prev => ({ ...prev, loop: !prev.loop }))}>
+    <ArrowUturnRightIcon className="toggle-btn-icon rotate-180" />
+  </button >;
 
   return (
-    <div className="lg:w-[85%] md:w-[95%] sm:w-[98%] w-[35rem] flex flex-col items-center p-2">
+    <div className="lg:w-[85%] md:w-[95%] sm:w-[98%] w-[35rem]flex flex-col items-center p-2">
       <TimeDisplay time={resultTime} />
 
       <div className="w-[100%] h-40 flex justify-center items-center rounded-2xl m-2 my-1">
-        {timerState === "init" && StartButton}
-        {timerState === "paused" && ResumeButton}
-        {timerState === "running" && PauseButton}
-        {timerState !== "init" && ResetButton}
-        {/* {StartButton}
-        {ResumeButton}
-        {PauseButton}
-        {ResetButton} */}
+        {timerState === "init" && startButton}
+        {timerState === "paused" && resumeButton}
+        {timerState === "running" && pauseButton}
+        {timerState !== "init" && resetButton}
       </div>
 
-      <div className="w-[100%] flex flex-col justify-center items-center bg-slate-500 rounded-2xl m-2 my-1">
-        <div>
-          <h4 className="text-white font-bold">Phase Config</h4>
-        </div>
-        <div>
-          <label htmlFor="auto-start" className="text-white">Start Next Phase Automatically: </label>
-          <input name="auto-start" type="checkbox" checked={settings.autostart_next_phase} onChange={() => setSettings(prev => ({ ...prev, autostart_next_phase: !prev.autostart_next_phase }))} />
-        </div>
+      <div className="w-[100%] flex flex-col justify-center items-center">
         <div className="flex">
-          {settings.quick_phases.map((qp, i) => (
-            <div key={i} className="flex items-center" onClick={() => setPhaseList(prev => [...prev, qp * 1000])}>
-              <div className="w-10 h-10 flex justify-center items-center p-2 m-2 bg-yellow-500 rounded-full">
-                <p className="text-white font-bold">{qp}</p>
-              </div>
-            </div>
-          ))}
+          {autoNextToggle}
+          {loopToggle}
         </div>
-        {phaseList.length ? <div className="w-[90%] min-h-20 rounded-2xl flex flex-wrap justify-start items-center bg-slate-900 p-2 m-2">
-          {phaseList.map((p, i) => (
-            <div key={i} className="flex items-center">
-              <div className={`w-10 h-10 flex justify-center items-center p-2 m-2 ${i === phase ? "bg-lime-600" : "bg-gray-400"} rounded-full`}>
-                <p className="text-white font-bold">{Math.round(p / 1000)}</p>
-              </div>
-              <p className="text-white font-bold"> &gt;&gt; </p>
 
-            </div>
-          ))}
-          <div onClick={() => setSettings(prev => ({ ...prev, loop: !prev.loop }))} className={`w-10 h-10 flex justify-center items-center p-2 m-2 ${settings.loop ? "bg-lime-600" : "bg-red-400"} rounded-full`}>
-            {settings.loop ? <i className="text-white font-bold"><ArrowUturnRightIcon className="w-7 h-7 text-white rotate-180" /></i> : null}
+        <div className="flex justify-center items-center my-2">
+
+          <div className="flex">
+            {settings.quick_phases.map((qp, i) => (
+              <div key={i} className="flex items-center" onClick={() => setPhaseList(prev => [...prev, qp * 1000])}>
+                <div className="w-10 h-10 flex justify-center items-center p-2 m-2 bg-yellow-500 rounded-full">
+                  <p className="text-white font-bold">{qp}</p>
+                </div>
+              </div>
+            ))}
           </div>
-        </div> : null}
-        <div className="flex justify-center items-center">
-          <input ref={inputRef} className=" w-16 h-10 text-center text-gray-800 rounded-l-xl focus:outline-none"
+
+          <input ref={inputRef} className="z-10 w-16 h-16 text-center text-gray-800 rounded-full focus:outline-none"
             placeholder="seconds" />
-          <button className="w-12 h-10 block border rounded-r-xl shadow-sm font-bold text-2xl  px-2 focus:outline-none focus:ring focus:ring-opacity-50 bg-indigo-500 hover:bg-indigo-700 text-white border-transparent focus:border-indigo-300 focus:ring-indigo-200 mr-2 mt-2 mb-2" onClick={handlePhaseInput}>
-            <p>+</p>
+          <button className="z-9 w-12 h-10 block border rounded-r-xl shadow-sm font-bold text-2xl  px-2 focus:outline-none focus:ring focus:ring-opacity-50 bg-violet-500 hover:bg-violet-700 text-white border-transparent focus:border-violet-300 focus:ring-indigo-200 ml-[-0.8rem] mr-2 mt-2 mb-2" onClick={handlePhaseInput}>
+            <PlusIcon className="ml-2 w-6 h-6" />
           </button>
         </div>
 
-
+        <div className="max-w-[90%] h-24 rounded-2xl flex flex-wrap justify-center items-center p-2 m-2">
+          {phaseList.map((p, i) => (
+            <div key={i} className="flex items-center" onClick={() => setPhaseList(prev => prev.filter((p, idx) => idx !== i))}>
+              <div className={`${i === phase ? "phase-active" : "phase-inactive"}`}>
+                <p className="text-white font-bold">{Math.round(p / 1000)}</p>
+              </div>
+              <p className="text-slate-200 font-bold"><ForwardIcon className="w-6 h-6" /> </p>
+            </div>
+          ))}
+          {phaseList.length ? <div className="flex items-center">{loopToggle}</div> : null}
+        </div>
 
       </div>
-
     </div>
-
-
   );
 };
 
